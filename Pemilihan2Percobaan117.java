09252d8dcc7cd46c11a63def7a8e8570a4f79054
muhammad hasbi ashiddiqi <muhammadhasbiasidiki175@gmail.com>
2024-10-04 21:57:32 +0700

    first commit

--- Pemilihan2Percobaan117.java
+++ Pemilihan2Percobaan117.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 /**
  * Pemilihan2Percobaan117
  */
+/**
+ * Pemilihan2Percobaan117
+ */
 public class Pemilihan2Percobaan117 {
-
     public static void main(String[] args) {
         Scanner D17 = new Scanner(System.in);
         System.out.print("masukkan tahun :");
         int tahun = D17.nextInt();
         if ((tahun % 4) == 0) {
-            if ((tahun % 100)!= 0) 
+            if (tahun % 100!= 0 || tahun % 400==0) 
             System.out.println("tahun kabisat ");
+
+            else
+            System.out.println("bukan tahun kabisat");
             
         } else {
             System.out.println("bukan tahun kabisat");
